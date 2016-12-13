3d8b647418d4e297f78c48d146554e9798e7d62f
maitabe <martushb@gmail.com>
2016-12-13 15:10:43 +0200

    login form

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var port = 8000;
 var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 
 app.use(passport.initialize());
 
-
+//create a global middleware
 							//verify callback function passing the arguments
 passport.use('login', new LocalStrategy(function (username, password, done) {
   var authenticated = username === "John" && password === "Smith";
@@ -22,3 +23,26 @@ passport.use('login', new LocalStrategy(function (username, password, done) {
   }
 }));
 
+
+//route-level middleware  apply only to the/login route
+					//passport.... middleware will use the first argument from passport.use 'login'
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/success',
+  failureRedirect: '/login',
+  session: false
+}));
+
+//success route
+app.get('/success', function (req, res){
+  res.send("Hey, hello from the server!");
+});
+
+//login  route
+app.get('/login', function (req, res) {
+  res.sendFile(__dirname + '/login.html');
+});
+
+
+app.listen(port, function() {
+	console.log('server started, listening on', port);
+});
