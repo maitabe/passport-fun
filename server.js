ac8094a261811ecf89663c2714cad25a8ce2d988
maitabe <martushb@gmail.com>
2016-12-13 14:31:07 +0200

    adding main files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser');
+
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+
+							//verify callback function passing the arguments
+passport.use('login', new LocalStrategy(function (username, password, done) {
+  var authenticated = username === "John" && password === "Smith";
+
+  if (authenticated) {
+    return done(null, { myUser:'user', myID: 1234 });
+  } else {
+    return done(null, false);
+  }
+}));
+
